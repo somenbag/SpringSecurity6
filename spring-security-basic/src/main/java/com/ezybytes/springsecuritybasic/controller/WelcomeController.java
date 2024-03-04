14a8312fa7503de093a8fb608a189e0a2cefe655
Somen Bag <somen.s@prodapt.com>
2024-03-04 13:01:27 +0530

    Create a Spring security project & secure Rest service

---
+++ spring-security-basic/src/main/java/com/ezybytes/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.ezybytes.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring Application with security";
+    }
+}
